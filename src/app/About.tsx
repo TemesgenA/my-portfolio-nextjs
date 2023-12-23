e11263728445257b95fa7b29e41341b4ec3fd8eb
Temesgen Ayneka <temesgen.ayneka@gmail.com>
2023-12-23 21:09:39 +0300

    Add About component

---
+++ src/app/About.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const AboutContainer = styled.div`
+  // Add your styles here
+`;
+
+export default function About() {
+    return (
+        <AboutContainer>
+            <h2>About Me</h2>
+            <p>Your introduction goes here</p>
+        </AboutContainer>
+    );
+}
