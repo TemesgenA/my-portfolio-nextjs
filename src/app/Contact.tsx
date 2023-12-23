e092e145ce81c0cb7e8099be74c75a8a83b5bbf4
Temesgen Ayneka <temesgen.ayneka@gmail.com>
2023-12-23 21:09:47 +0300

    Add Contact component with styled container

---
+++ src/app/Contact.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const ContactContainer = styled.div`
+  // Add your styles here
+`;
+
+export default function Contact() {
+    return (
+        <ContactContainer>
+            <h2>Contact</h2>
+            <p>Email: your-email@example.com</p>
+            <p>LinkedIn: <a href="https://linkedin.com/in/yourusername">yourusername</a></p>
+            <p>GitHub: <a href="https://github.com/yourusername">yourusername</a></p>
+            {/* Add a contact form here if you want */}
+        </ContactContainer>
+    );
+}
