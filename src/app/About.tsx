5f14d4f5f2c6601b031835533029df7a0730288e
Temesgen Ayneka <temesgen.ayneka@gmail.com>
2023-12-27 20:44:22 +0300

    Update About component with developer introduction

--- src/app/About.tsx
+++ src/app/About.tsx
@@ -8,7 +8,31 @@ export default function About() {
     return (
         <AboutContainer>
             <h2>About Me</h2>
-            <p>Your introduction goes here</p>
+            <p>
+                I am a full stack web developer and mobile developer with a passion for creating innovative and user-friendly applications.
+                With 3 years of experience in the industry, I have developed a strong foundation in both frontend and backend technologies.
+            </p>
+            <p>
+                My expertise lies in building scalable and performant web applications using modern frameworks like React.
+                I am proficient in HTML, CSS, and JavaScript, and have a deep understanding of responsive design principles.
+            </p>
+            <p>
+                On the backend, I have worked extensively with Node.js and Express to develop RESTful APIs and handle database operations.
+                I am also experienced in working with databases such as MongoDB and MySQL.
+            </p>
+            <p>
+                In addition to Node.js, I have experience with the Django framework for building robust and scalable web applications.
+                I have worked with Django&apos;s ORM to handle database operations and create RESTful APIs.
+                I am proficient in Python and have a strong understanding of Django&apos;s architecture and best practices.
+            </p>
+            <p>
+                In addition to web development, I have a strong interest in mobile app development.
+                I have built native Android applications using Kotlin, which allows me to leverage my existing web development skills to create native experiences for Android.
+            </p>
+            <p>
+                I am a highly motivated and detail-oriented developer, always striving to stay up-to-date with the latest technologies and best practices.
+                I enjoy working in collaborative environments and thrive in fast-paced, deadline-driven projects.
+            </p>
         </AboutContainer>
     );
 }
