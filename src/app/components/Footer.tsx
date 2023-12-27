b45670835d653d91e1cdeae29c49d7f2a7416528
Temesgen Ayneka <temesgen.ayneka@gmail.com>
2023-12-27 21:18:41 +0300

    Add Footer component

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            <p>This is the footer of my portfolio website.</p>
+            <ul>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </footer>
+    );
+};
+
+export default Footer;
